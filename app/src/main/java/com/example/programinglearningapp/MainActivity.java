53dc6731861d3d63034bb75e640db4aa7ca970eb
Nguyenvanhieu2403 <20212107@eaut.edu.vn>
2024-09-07 13:47:05 +0700

    feat: navigate member management

--- app/src/main/java/com/example/programinglearningapp/MainActivity.java
+++ app/src/main/java/com/example/programinglearningapp/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.programinglearningapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 
+import com.example.programinglearningapp.ui.MemberManagement;
 import com.example.programinglearningapp.ui.course.CourseListFragment;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
+import androidx.core.view.GravityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -44,12 +49,41 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_infor_personal, R.id.nav_infor_course,R.id.nav_log_out)
+                R.id.nav_home, R.id.nav_infor_personal, R.id.nav_infor_course, R.id.nav_member_management,R.id.nav_log_out)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+
+                if (id == R.id.nav_member_management) {
+                    // Mở MemberManagementActivity
+                    Intent intent = new Intent(MainActivity.this, MemberManagement.class);
+                    startActivity(intent);
+                } else {
+                    // Xử lý các mục khác
+                    NavController navController = Navigation.findNavController(MainActivity.this, R.id.nav_host_fragment_content_main);
+                    if (id == R.id.nav_home) {
+                        navController.navigate(R.id.nav_home);
+                    } else if (id == R.id.nav_infor_personal) {
+                        navController.navigate(R.id.nav_infor_personal);
+                    } else if (id == R.id.nav_infor_course) {
+                        navController.navigate(R.id.nav_infor_course);
+                    } else if (id == R.id.nav_log_out) {
+                        // Xử lý đăng xuất
+                    }
+                }
+
+                DrawerLayout drawer = binding.drawerLayout;
+                drawer.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
 
 //        loadHomeFragment();
     }
